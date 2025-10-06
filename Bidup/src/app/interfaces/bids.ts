417df9446bae7ae33f577b645eba66b686b17d43
Fabo <fabianquiro05@hotmail.com>
2025-10-06 14:19:30 -0500

    Inicio de codigo con la base dejada en main

--- Bidup/src/app/interfaces/bids.ts
+++ Bidup/src/app/interfaces/bids.ts
@@ -1,2 +1,6 @@
-export interface Bids {
+export interface Bid {
+  _id?: string;             // ID generado por MongoDB
+  product: string;          // ObjectId 
+  bidder: string;           // ObjectId 
+  amount: number;           // Monto de la oferta    
 }
--- Bidup/src/app/interfaces/credencials.ts
+++ Bidup/src/app/interfaces/credencials.ts
@@ -1,2 +1,4 @@
 export interface Credencials {
+    email: string;
+    password: string;   
 }
