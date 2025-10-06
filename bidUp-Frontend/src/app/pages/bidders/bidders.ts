fc2d0eaf928d3c6e34a4d4351bc376a2b55e0678
izzy076 <elirodri17002@gmail.com>
2025-10-06 17:52:31 -0500

    first commit

---
+++ bidUp-Frontend/src/app/pages/bidders/bidders.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bidders',
+  imports: [],
+  templateUrl: './bidders.html',
+  styleUrl: './bidders.css'
+})
+export class Bidders {
+
+}
